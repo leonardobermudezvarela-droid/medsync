511163aa1c8aff0589b2329642ae99064aef9f3a
MedSync <leonardo.bermudez.varela@gmail.com>
2026-07-03 09:41:46 -0600

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,22 @@
+// Service Worker básico para permitir la instalación PWA de MedSync Tracker
+const CACHE_NAME = 'medsync-v1';
+const ASSETS = [
+  'index.html',
+  'manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
